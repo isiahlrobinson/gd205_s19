bfc44a8cc645bd70551ccb913649cc08186af8d3
isiah <isiahlrobinson@gmail.com>
2019-02-13 16:41:14 -0500

    game board

---
+++ Assets/scripts/boardmovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardmovement : MonoBehaviour {
+
+    public float gridSize = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("im here");
+        transform.position += transform.forward * gridSize;
+    }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("im here");
+            transform.position += -transform.right * gridSize;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("im here");
+            transform.position += -transform.forward * gridSize;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("im here");
+            transform.position += transform.right * gridSize;
+        }
+
+
+    }
+}
